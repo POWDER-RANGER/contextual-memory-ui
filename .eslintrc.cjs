efcd965d43d4ea9e278fc64045e8a09040d10d9e
Full-Stack Developer | AI/ML Systems | Cybersecurity | Game Dev (UE5) | Building @nine-realities-netcode | ORCID: 0009-0008-9273-2458 <137366958+POWDER-RANGER@users.noreply.github.com>
2025-12-13 14:42:13 -0600

    Add ESLint configuration for React project

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-hooks', 'react-refresh'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'warn',
+    'no-unused-vars': 'warn',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
